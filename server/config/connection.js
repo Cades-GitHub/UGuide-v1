a29016b6c246e188b70b1a51f443001629ec1483
Cade Jones <jonescade112@gmail.com>
2024-05-14 22:15:47 -0500

    changed connection again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/UGuide');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UGuide');
 
 module.exports = mongoose.connection;
